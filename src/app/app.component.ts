e0fe8ed4aeab4b58b0589df15ce1a708a711d698
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-22 10:08:14 +0530

    counter store added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-// import { Store } from '@ngrx/store'
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store'
+import { decrement, increament } from './store/counter.reducer';
+import {MyAppState} from './store/app.state'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-deployment';
+  myCounter: number = 0;
+
+  constructor(private store: Store<MyAppState>){}
+
+  ngOnInit(): void {
+    this.store.subscribe((state)=>{
+      console.log(state);
+      this.myCounter = state.counter;
+    })
+  }
+
+  increamentCounter(){
+    this.store.dispatch(increament());
+  }
+
+  decreamentCounter(){
+    this.store.dispatch(decrement());
+  }
 }
---
+++ src/app/store/app.state.ts
@@ -0,0 +1,3 @@
+export interface MyAppState{
+  counter: number
+}
